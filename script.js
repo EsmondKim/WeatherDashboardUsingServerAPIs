4dfa241641d216e58f6d7c35f0f0e1f1926041d0
EsmondKim <ekelectronicmail@gmail.com>
2020-11-08 14:23:14 -0600

    Getting AJAX from weather api to print to console for Boston.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
     console.log(moment().format());
     console.log(moment().hours());
+
+    let queryURLCurrentWthr = "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=87665d60f93b269a6c1aa2b881ea7347";
     
+    $.ajax({
+        url: queryURLCurrentWthr,
+        method: "GET"
+      }).then(function(response) {
+      console.log(response); 
+      })
+    
+
+
+
 })
