8eab5387b12906fbdb784249d62b32bafd265c54
EsmondKim <ekelectronicmail@gmail.com>
2020-11-08 15:29:25 -0600

    Added jquery .val to script.js to capture search for a city info.

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ $(document).ready(function() {
     console.log(moment().format());
     console.log(moment().hours());
 
-    let queryCity = ""
-    let queryURLCurrentWthr = "https://api.openweathermap.org/data/2.5/weather?q=" + queryCity + "&appid=87665d60f93b269a6c1aa2b881ea7347";
     
     $("#btn").on("click", getWeather)
 
     $(document).keypress(function(event){
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if(keycode == '13'){
-            alert('You pressed a "enter" key in somewhere');    
+            console.log('You pressed "enter"'); 
+            getWeather();
         }
     });
 
     function getWeather() {
+    console.log("You hit search btn.");
+    let queryCity = $("#searchText").val(); 
+    let queryURLCurrentWthr = "https://api.openweathermap.org/data/2.5/weather?q=" + queryCity + "&appid=87665d60f93b269a6c1aa2b881ea7347";
+    alert(queryCity);
     $.ajax({
         url: queryURLCurrentWthr,
         method: "GET"
