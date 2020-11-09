f1fb9b6ba750242a6e9b545deb4b7737def14986
EsmondKim <ekelectronicmail@gmail.com>
2020-11-08 20:57:45 -0600

    Added queryURL for UV Index and logging UV Index object to console.

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
         let lastSearched = JSON.parse(window.localStorage.getItem("citySearched"));
         $(':input').removeAttr('placeholder');
         $("#searchText").val(lastSearched);
-        //alert(lastSearched);
     }
     getFromLocalStorage()
 
@@ -22,9 +21,7 @@ $(document).ready(function() {
     function getWeather() {
     let queryCity = $("#searchText").val(); 
     let queryURLCurrentWthr = "https://api.openweathermap.org/data/2.5/weather?q=" + queryCity + "&appid=87665d60f93b269a6c1aa2b881ea7347";
-    //console.log(queryCity);
     window.localStorage.setItem("citySearched", JSON.stringify($("#searchText").val()))
-    
     $("#citiesList").append('<li class="list-group-item">' + queryCity + "</li>")
 
     $.ajax({
@@ -36,19 +33,33 @@ $(document).ready(function() {
       let tempF = (response.main.temp - 273.15) * 1.80 + 32;
       let humidity = response.main.humidity;
       let wind = response.wind.speed;
-      let uv = response.
       $("#cityName").text(response.name);
       $("#cityTime").text(moment().format("MMM Do YY"));
       $("#cityTemp").text(tempF.toFixed(0) + " degrees Farenheit");
       $("#cityHumidity").text("Humidity: " + humidity); 
       $("#cityWind").text("Wind Speed: " + wind);
-      $("#uvIndex").text("UV Index: " + uv);  
-      })
-  
+   
+      let uvLat = response.coord.lat;
+      let uvLon = response.coord.lon;
+      console.log(uvLat);
+      console.log(uvLon);
+      let queryURLUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + uvLat + "&lon=" + uvLon + "&appid=87665d60f93b269a6c1aa2b881ea7347";
 
+      $.ajax({
+        url: queryURLUV,
+        method: "GET"
+      }).then(function(response) {
+      console.log(response); 
+   
+      
+      })
+
+
+    })
     // and the UV index
     //an icon representation of weather conditions,
 
+   
     }
 
 
