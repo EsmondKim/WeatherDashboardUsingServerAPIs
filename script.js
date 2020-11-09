bce86ca9fc492d95386b458bb87484649d38484e
EsmondKim <ekelectronicmail@gmail.com>
2020-11-09 02:04:00 -0600

    Completed five day forecast card.

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
     let queryCity = $("#searchText").val(); 
     let queryURLCurrentWthr = "https://api.openweathermap.org/data/2.5/weather?q=" + queryCity + "&appid=87665d60f93b269a6c1aa2b881ea7347";
     window.localStorage.setItem("citySearched", JSON.stringify($("#searchText").val()))
-    $("#citiesList").append('<li class="list-group-item">' + queryCity + "</li>")
+    $("#citiesList").append('<li style="text-align: center;" class="list-group-item">' + queryCity + "</li>")
 
     $.ajax({
         url: queryURLCurrentWthr,
@@ -84,38 +84,62 @@ $(document).ready(function() {
       let day1Temp = response.daily[1].temp.day;
       let day1TempToF = (day1Temp - 273.15) * 1.80 + 32;
       let day1Humidity = response.daily[1].humidity;
-      console.log(day1Humidity)
       
-      //let unixTSDay2 = response.daily[2].dt;
-      //let day2Milli = unixTSDay2 * 1000;
-      //let day2DateObj = new Date(day2Milli);
-     // let hmnDay2 = day2DateObj.toDateString();
-     // let day2Temp = response.daily[2].temp;
+      let unixTSDay2 = response.daily[2].dt;
+      let day2Milli = unixTSDay2 * 1000;
+      let day2DateObj = new Date(day2Milli);
+      let hmnDay2 = day2DateObj.toDateString();
+      let day2Temp = response.daily[2].temp.day;
+      let day2TempToF = (day2Temp - 273.15) * 1.80 + 32;
+      let day2Humidity = response.daily[2].humidity;
 
-      //let unixTSDay3 = response.daily[3].dt;
-      //let day3Milli = unixTSDay3 * 1000;
-      //let day3DateObj = new Date(day3Milli);
-      //let hmnDay3 = day3DateObj.toDateString();
-      //let day3Temp = response.daily[3].temp;
+      let unixTSDay3 = response.daily[3].dt;
+      let day3Milli = unixTSDay3 * 1000;
+      let day3DateObj = new Date(day3Milli);
+      let hmnDay3 = day3DateObj.toDateString();
+      let day3Temp = response.daily[3].temp.day;
+      let day3TempToF = (day3Temp - 273.15) * 1.80 + 32;
+      let day3Humidity = response.daily[3].humidity;
 
-      //let unixTSDay4 = response.daily[4].dt;
-      //let day4Milli = unixTSDay4 * 1000;
-      //let day4DateObj = new Date(day4Milli);
-      //let hmnDay4 = day4DateObj.toDateString();
-      //let day4Temp = response.daily[4].temp;
+      let unixTSDay4 = response.daily[4].dt;
+      let day4Milli = unixTSDay4 * 1000;
+      let day4DateObj = new Date(day4Milli);
+      let hmnDay4 = day4DateObj.toDateString();
+      let day4Temp = response.daily[4].temp.day
+      let day4TempToF = (day4Temp - 273.15) * 1.80 + 32;
+      let day4Humidity = response.daily[4].humidity;
 
-      //let unixTSDay5 = response.daily[5].dt;
-      //let day5Milli = unixTSDay5 * 1000;
-      //let day5DateObj = new Date(day5Milli);
-      //let hmnDay5 = day5DateObj.toDateString();
-      //let day5Temp = response.daily[5].temp;
+      let unixTSDay5 = response.daily[5].dt;
+      let day5Milli = unixTSDay5 * 1000;
+      let day5DateObj = new Date(day5Milli);
+      let hmnDay5 = day5DateObj.toDateString();
+      let day5Temp = response.daily[5].temp.day;
+      let day5TempToF = (day5Temp - 273.15) * 1.80 + 32;
+      let day5Humidity = response.daily[5].humidity;
       
-      $("#dayOne").text("Date: " + hmnDay1 + " Temp: " + day1TempToF.toFixed(0) + " Humidity: " + day1Humidity);
-      //$("#dayTwo").text(hmnDay2 + );
-      //$("#dayThree").text(hmnDay3 + );
-      //$("#dayFour").text(hmnDay4 + ); 
-      //$("#dayFive").text(hmnDay5 + );
+      $("#dayOne").text("DATE: " + hmnDay1 + ";" + " TEMP: " + day1TempToF.toFixed(0) + ";" +" HUMIDITY: " + day1Humidity);
+      $("#dayTwo").text("DATE: " + hmnDay2 + ";" + " TEMP: " + day2TempToF.toFixed(0) + ";" +" HUMIDITY: " + day2Humidity);
+      $("#dayThree").text("DATE: " + hmnDay3 + ";" + " TEMP: " + day3TempToF.toFixed(0) + ";" +" HUMIDITY: " + day3Humidity);
+      $("#dayFour").text("DATE: " + hmnDay4 + ";" + " TEMP: " + day4TempToF.toFixed(0) + ";" +" HUMIDITY: " + day4Humidity); 
+      $("#dayFive").text("DATE: " + hmnDay5 + ";" + " TEMP: " + day5TempToF.toFixed(0) + ";" +" HUMIDITY: " + day5Humidity);
                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //DO NOT CHANGE BRACKETS BELOW!!!!      
                 })  
             })
